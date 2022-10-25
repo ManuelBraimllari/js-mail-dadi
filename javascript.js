1e6380b01482271e8f5ef158e3d4d9cd101c0436
ManuelBraimllari <manubra999@gmail.com>
2022-10-26 01:23:01 +0200

    start

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const emailList =['paperino@gmail.com','pippo@gmail.com','pluto@gmail.com'];
+const userEmail = prompt('Inserisci la tua email');
+
+if(userEmail == emailList){
+    alert("Bentornato")
+    
+}else{
+    alert("Prova di nuovo")
+}
+
