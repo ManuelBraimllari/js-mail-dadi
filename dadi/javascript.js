8346df4f3b2e6a0dbc10cd76baa1e822dcf627cb
ManuelBraimllari <manubra999@gmail.com>
2022-10-26 10:57:56 +0200

    finish

---
+++ dadi/javascript.js
@@ -0,0 +1,26 @@
+const eleNumPlayer = document.querySelector('#num-player')
+const eleNumComputer = document.querySelector('#num-computer')
+const eleOutput = document.querySelector('#output')
+
+
+
+
+
+        const NumPlayer = Math.floor(Math.random()*6)+1;
+        const NumComputer = Math.floor(Math.random()*6)+1;
+
+        eleNumPlayer.innerHTML = NumPlayer
+        eleNumComputer.innerHTML = NumComputer
+
+        let message;
+
+        if (NumPlayer > NumComputer){
+            message = 'Bravo hai vinto'
+        }else if (NumPlayer < NumComputer){
+            message ='Hai perso, prova di nuovo'
+        }else {
+            message = 'Avete pareggiato, prova di nuovo'
+        }
+
+        eleOutput.innerHTML = message
+
